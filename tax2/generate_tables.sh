f6f77e65ff4045e189ab11f65abafe5f3789de29
Kevin Harlan <kcharlan@gmail.com>
2025-12-10 16:22:04 -0500

    Updates to table generation
    Changed over hard codes to 2026.

--- tax2/generate_tables.sh
+++ tax2/generate_tables.sh
@@ -2,17 +2,17 @@
 
 source venv/bin/activate
 
-python cli.py rules/federal/2025.yaml \
+python cli.py --rules rules/federal/2026.yaml \
   --filing-status single \
   --inc-max 500000 \
   --step 50 \
-  --out tables/federal_2025.parquet
+  --out tables/federal_2026.parquet
 
-python cli.py rules/states/GA/2025.yaml \
+python cli.py --rules rules/states/GA/2026.yaml \
   --filing-status single \
   --inc-max 500000 \
   --step 50 \
-  --out tables/ga_2025.parquet
+  --out tables/ga_2026.parquet
 
 python3 merge_tables.py
 
